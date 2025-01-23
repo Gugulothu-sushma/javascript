92553ea690d9ef10ceea15a1808ce041195995f4
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-23 14:05:50 +0000

    converstionobject.js

---
+++ 1.bascis/3.converstionobject.js
@@ -0,0 +1,19 @@
+let score="33abc"
+let score1=true
+let score2=undefined
+let valueInNumber=Number(score)
+let valueInNumber1=Number(score1)
+
+console.log(typeof valueInNumber)
+console.log(typeof true)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+//for easily converstion boolean
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+let someNumber=44
+let stringNumber=String(someNumber)
+console.log(typeof 44)
+console.log(stringNumber)

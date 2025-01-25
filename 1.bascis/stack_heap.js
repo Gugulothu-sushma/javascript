54a4e49bd6fdcd3e1a51dbd004495c7bc84058aa
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-25 10:38:04 +0000

    stack_heap.js

---
+++ 1.bascis/stack_heap.js
@@ -0,0 +1,17 @@
+//stack
+
+let myyoutubename="sushma"
+
+let anothername=myyoutubename
+ anothername="xyz"
+console.log(myyoutubename)
+console.log(anothername)
+//heap
+let userone={
+     email:"sushma@2",
+     id:"123"   
+}
+let usertwo=userone
+ usertwo.email="varma@12"
+ console.log(userone.email)
+console.log(usertwo.email)

1b1d7ffb92885764deccc12b538acd50c1aa3271
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-25 10:08:38 +0000

    converstionobject.js

--- 1.bascis/3.converstionobject.js
+++ 1.bascis/3.converstionobject.js
@@ -15,5 +15,28 @@ console.log(booleanisLoggedIn)
 
 let someNumber=44
 let stringNumber=String(someNumber)
-console.log(typeof 44)
+console.log(typeof 44 )
 console.log(stringNumber)
+
+//opertions
+//let value=3
+let negvalue=-3
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2**3)
+console.log(2-3)
+
+let string1="hello"
+let string2="sushma"
+
+let string3=(string1 + string2)
+console.log(string3) 
+console.log("2"+5)
+console.log(2+2+"1"+2)
+console.log(true)
+console.log(+true)
+
+let gamescore=100
+++gamescore;//prefix increase values
+console.log(gamescore);

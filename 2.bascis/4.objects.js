20f227a3ec199d56ff1056b9fa8ba2b9d2834a26
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-28 11:45:57 +0000

    4.objects.js

---
+++ 2.bascis/4.objects.js
@@ -0,0 +1,34 @@
+//const tinderuser=new Object()
+
+const tinderuser={}
+tinderuser.id="1234"
+tinderuser.name="sus"
+tinderuser.isLoggedIn=false
+console.log(tinderuser)
+
+
+//one obect in another object//
+const regularuser={
+    email:"sus2@gmail.com",
+    fullname:{
+        username:{
+            firstname:"sus",
+            sceondname:"gugu"
+        }
+    }
+
+}
+console.log(regularuser.fullname.username.firstname);
+
+
+//merge objects//
+const obj1={1:"a",2:"g"}
+const obj2={3:"s",4:"u"}
+//const obj3={obj1,obj2}
+const obj3=Object.assign({},obj1,obj2)//(target,source),we consider this {} empty target.
+console.log(obj3);
+
+//array in object
+console.log(Object.keys(tinderuser))
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))

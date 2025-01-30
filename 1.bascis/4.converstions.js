ad86141f28185195ebbe3ede109933796c17beef
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-30 14:10:36 +0000

    4.converstions.js

---
+++ 1.bascis/4.converstions.js
@@ -0,0 +1,35 @@
+let score="33abc"
+let score1=true
+let score2=undefined
+let valueInNumber=Number(score)
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber)
+console.log(typeof true)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+//for easily converstion boolean
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+let someNumber=44
+let stringNumber=String(someNumber)
+console.log(typeof 44 )
+console.log(stringNumber)
+//opertions
+//let value=3
+let negvalue=-3
+console.log(negvalue)
+console.log(2+2)
+console.log(2**3)
+console.log(2-3)
+let string1="hello"
+let string2="sushma"
+let string3=(string1 + string2)
+console.log(string3) 
+console.log("2"+5)
+console.log(2+2+"1"+2)
+console.log(true)
+console.log(+true)
+let gamescore=100
+++gamescore;//prefix increase values
+console.log(gamescore);

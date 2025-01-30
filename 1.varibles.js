7abad12abbd1339005c303c218bf5e5471e7cc0b
Gugulothu-sushma <gugulothusushma9@gmail.com>
2025-01-30 14:04:15 +0000

    1.varible.js

---
+++ 1.varibles.js
@@ -0,0 +1,10 @@
+const accountID=12345
+let accountEmail="gugulothusushma@9gmail.com"
+var accountPassword="123"
+ accountCity="jaipur"
+ 
+ let accountState;
+//accountID=3
+console.log(accountID);
+console.log(accountEmail)
+console.table([accountID,accountPassword,accountEmail,accountCity,accountState])
